3f33d4ace4bf15fb0bdddb2f43a86ca6ff533629
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-01 12:08:44 +0300

    add: CRUD operations

--- student_api/views.py
+++ student_api/views.py
@@ -26,3 +26,35 @@ def student_api(request):
             }
             return Response(data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE','PATCH'])
+def student_api_get_update_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method =='GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "message" : f"Student {student.last_name} updated successfully"
+            }
+            return Response(data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PATCH':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "message": f"Student {student.last_name} updated succesfully"
+            }
+            return Response(data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        student.delete()
+        data = {
+            "message": f"Student {student.last_name} deleted successfully"
+        }
+        return Response(data)
+            
