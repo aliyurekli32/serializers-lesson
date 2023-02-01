66101de3de05d8da0196866aaec9ddfc07a8d631
aliyurekli32 <adamjesperblack@gmail.com>
2023-02-01 11:51:17 +0300

    add: @apiview get,post

--- student_api/views.py
+++ student_api/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
+def home(request):
+    return HttpResponse('<h1>Serializer</h1>')
 
+@api_view(['GET','POSt'])
+def student_api(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        print(serializer)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "message": f"Student {serializer.validated_data.get('first_name')} saved succesfully "
+            }
+            return Response(data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
